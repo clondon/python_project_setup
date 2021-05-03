f8487165f09bec61652be30adc85bd32806803a1
Charles London <charles@new-web-sites.com>
2021-05-03 17:22:54 +0100

    Added target directory checks

--- copy_python_skel.sh
+++ copy_python_skel.sh
@@ -4,15 +4,35 @@
 PROJ_DIR="~/Documents/python_proj/test_dir"
 # Do not use spce when assigning variables, Do not use the $ when assigning variables
 
-
 DEBUG="false"
-echo "New directory is {$PROJ_DIR}"
 
+
+
+# Check target directory
+if [ -d "$PROJ_DIR" ]
+then
+    echo "Target directory exists, not creating!"
+ 
+else
+    echo "Directory does not exist, creating directory $PROJ_DIR"
+    tree /f "$PROJ_DIR"
+    mkdir -p "$PROJ_DIR"
+    if [ $? -eq == 0 ]
+    then
+        echo "Directory was created"
+    fi
+fi
+
+
+
+exit 0
 echo "Creating Python project in ~/Documents/Projects/python_proj/skeleton/* ~/Documents/Projects/python_proj/$1."
+
+
 
 CMDLINEARG=$@
 
-if [ $# -lt 2 ] # Test if the user enter a command lin argument
+if [ $# -lt 2 ] # Test if the user entered a command line argument.
     then 
         echo "Command line arguments required."
         echo "Usage $0 \"new_dir_name\""
